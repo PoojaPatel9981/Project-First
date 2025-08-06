5e6eeb965491215ec6738b78a71e6ed798124751
Mohit Patel <patelmohitpatelmohit7067@gmail.com>
2025-08-06 22:00:15 +0530

    update LoopPractice and SumOfTwoNUmber files

--- src/SumOfTwoNumber.java
+++ src/SumOfTwoNumber.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class SumOfTwoNumber
 {
     public static void main(String[] args) {
-        int num1,num2,result;
+
 
         Scanner input=new Scanner(System.in);
 
         System.out.println("enter number first : ");
-        num1=input.nextInt();
+        int num1=input.nextInt();
 
         System.out.println("enter number second : ");
-        num2=input.nextInt();
+        int num2=input.nextInt();
 
-        result=num1 + num2;
+        int result=num1 + num2;
         System.out.println("the sum of two number is : " + result);
     }
 }
