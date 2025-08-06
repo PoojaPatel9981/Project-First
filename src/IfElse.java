84a76fb4cc8743993f9344de02193a7441408240
Mohit Patel <patelmohitpatelmohit7067@gmail.com>
2025-08-06 22:16:26 +0530

    Update IfElse.java with basic If-Else code

--- src/IfElse.java
+++ src/IfElse.java
@@ -1,21 +1,11 @@
-import java.util.Scanner;
-
-public class IfElse
-{
+public class IfElse {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        int number = 5;
 
-        int age;
-        System.out.println("Enter your age : ");
-        age=sc.nextInt();
-
-        if(age >= 18)
-        {
-            System.out.println("you are eligable in vote");
-        }
-        else
-        {
-            System.out.println("you are not eligable in vote");
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
         }
     }
 }
